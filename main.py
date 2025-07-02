8718f63ebcca72b97f7e32f3677af4253555f10d
AviKohn7 <avirosenberg123@gmail.com>
2025-07-01 21:54:56 -0400

    microcommit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from sys import argv
 
 app = Flask(__name__)
 
@@ -11,19 +12,19 @@ def form():
     if request.method == 'POST':
         programs = request.form.getlist('programs')
         if not programs:
-            # Redirect back to home if no programs were selected
+            # Redirect back to home if no programs were selected. Needed if you navigate directly to it without using index.html first.
             return redirect(url_for('index'))
         return render_template('form.html', programs=programs)
     # If accessed via GET, redirect to home
     return redirect(url_for('index'))
 
-@app.route('/submit', methods=['POST'])
+@app.route('/send-request', methods=['POST'])
 def submit():
-    programs = request.form.get('programs').split(',')
+    programs = request.form.get('programs').split(',') #todo: verify values (in the method ig, best place)
     # You can now access the list of selected programs
     # and the other form data to process the request.
     print(programs) # For debugging
     return render_template('request-received.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug= (True if len(argv) > 1 else False)) #use debug if any commmand line arguments are inputted
