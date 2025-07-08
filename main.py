04064fba3d87c57aa6bc4e5842c8d302c0c1c8a1
AviKohn7 <avirosenberg123@gmail.com>
2025-07-08 19:59:56 -0400

    Basic Server complete for TFOFinder downloading.

--- main.py
+++ main.py
@@ -1,17 +1,83 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sys import argv
 from src.TFOFinder import tfofinder
-
+from werkzeug.utils import secure_filename
+from functools import partial
+import time, io, zipfile, base64
+from pathlib import Path
 
 app = Flask(__name__)
-program_dict = { #get args, validate args, return value
-    'tfofinder': (lambda req: (request.form.get("tfo-probe-length")),
-                tfofinder.validate_arguments,
-                  tfofinder.calculate_result), #temp functions
-    'smfish': (lambda x: x, lambda x: x),
-    'pinmol': (lambda x: x, lambda x: x),
-}
 
+class Program:
+    def _validate_args(self, kwargs, error_message="Something went wrong"):
+        try:
+            return self._validate_args_raw(**kwargs)
+        except TypeError as e:
+            return TypeError(f"{error_message}: {str(e)}")
+
+    def _run_program(self, kwargs, error_message="Something went wrong"):
+        try:
+            return self._run_program_raw(**kwargs)
+        except Exception as e:
+            return Exception(f"{error_message}: {str(e)}")
+
+    def run(self, kwargs, validate_err_msg, runtime_err_msg):
+        self._validate_args(kwargs, validate_err_msg)
+        result =  self._run_program(kwargs, runtime_err_msg)
+        return self._get_response(result, **kwargs)
+
+    def __init__ (self, get_args, validate_args, run_program, get_response):
+        self.get_args = get_args
+        self._validate_args_raw = validate_args
+        self._run_program_raw = run_program
+        self._get_response = get_response
+
+def close_file(func, *, file_index = "filein", **kwargs):
+    """
+    Execute a function and close the file afterward.
+    :param func: the function to execute
+    :param args: the arguments to pass to that function.
+    :param file_index: the index of the argument representing the file to be closed
+    :return:
+    """
+    with kwargs[file_index]:
+        return func(**kwargs)
+
+def get_zip(**kwargs):
+    file_obj = io.BytesIO()
+    with zipfile.ZipFile(file_obj, 'w', compression=zipfile.ZIP_DEFLATED) as zip_file:
+        for name, value in kwargs.items():
+            zip_info = zipfile.ZipInfo(name)
+            zip_info.date_time = time.localtime(time.time())[:6]
+            zip_file.writestr(name + value[0], value[1])
+    file_obj.seek(0)
+    return file_obj
+
+def get_tfofinder_result(result, **kwargs):
+    zip_file = get_zip(TFOProbes = (".txt", result))
+    zip_file_encoded = base64.b64encode(zip_file.getvalue()).decode("utf-8")
+    filename = Path(kwargs["filename"]).stem
+    filename = f"TFOFinderResultsFor-{filename}.zip"
+    return jsonify(zip=zip_file_encoded, html=render_template(
+        'request-completed.html', result=result, program = "TFOFinder", filename=filename))
+
+def get_result_temp(program, result, **kwargs):
+    zip_file = get_zip(**{program: (".txt", result)})
+    zip_file_encoded = base64.b64encode(zip_file.getvalue()).decode("utf-8")
+    filename = f"TempResult-{program}.zip"
+    return jsonify(zip=zip_file_encoded, html=render_template(
+        'request-completed.html', result=result, program = program, filename=filename))
+
+program_dict = { #get args, validate args, return value
+    'tfofinder': Program(lambda req: {"filein": req.files.get("ct-file").stream,
+                                      "probe_length": req.form.get("tfofinder-probe-length", type=int),
+                                      "filename": secure_filename(req.files.get("ct-file").filename)},
+                tfofinder.validate_arguments,
+                  partial(close_file, tfofinder.calculate_result),
+                         get_tfofinder_result), #temp functions
+    'smfish': Program(lambda x: {"x": "test"}, lambda x: True, lambda x: x, partial(get_result_temp, "smFISH")),
+    'pinmol': Program(lambda x: {"x": "test"}, lambda x: True, lambda x: x, partial(get_result_temp, "PinMol"))
+}
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,23 +89,21 @@ def form():
         if not programs:
             # Redirect back to home if no programs were selected. Needed if you navigate directly to it without using index.html first.
             return redirect(url_for('index'))
-        return render_template('request_form.html', programs=programs)
+        return render_template('request.html', programs=programs)
     # If accessed via GET, redirect to home
     return redirect(url_for('index'))
 
 @app.route('/send-request', methods=['POST'])
 def send_request():
-    programs = request.form.get('programs').split(',')
-    # You can now access the list of selected programs
-    # and the other form data to process the request.
-    print(request.form) # For debugging
-    file = request.files.get("ct-file")
+    program = request.args.get("program")
+    return run_program(program, "The given arguments are invalid",
+                         "Something went wrong when calculating your result")
 
-    for prog_name in programs:
-        args = program_dict[prog_name][0](request)
-        if program_dict[prog_name][1](*args): program_dict[prog_name][2](*args)
-        else : pass #todo: send fail message
-    return render_template('request-received.html')
+
+def run_program(prog_name, error_message_validation="Something went wrong", error_message_program="Something went wrong"):
+    program = program_dict[prog_name.lower()]
+    kwargs = program.get_args(request)
+    return program.run(kwargs, error_message_validation, error_message_program)
 
 if __name__ == '__main__':
     app.run(debug= (True if len(argv) > 1 else False)) #use debug if any commmand line arguments are inputted
--- src/TFOFinder/tfofinder.py
+++ src/TFOFinder/tfofinder.py
@@ -1,9 +1,10 @@
 import sys
-
+import os
 import pandas as pd
 from Bio.SeqUtils import MeltingTemp as mt
 from pathlib import Path
 from Bio.Seq import Seq #don't think this is used even in the original
+from numpy.f2py.rules import arg_rules
 from pandas import DataFrame
 
 sys.path.append(str(Path(__file__).parent.parent))
@@ -12,15 +13,31 @@ from util import input_int_in_range
 probeMin = 4
 probeMax = 30
 match = ["ENERGY", "dG"] #find header rows in ct file
+base_complement = str.maketrans({'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'})
 
-def validate_arguments(probe_length: int, file=None) -> bool:
-    return type(probe_length) is int and (probeMin <= probe_length <= probeMax)
 
-def calculate_result(probe_length: int, filein=None, filename=None, arguments = None):
+def validate_arguments(filein, probe_length: int, filename="") -> bool:
+    if type(probe_length) is not int or not (probeMin <= probe_length <= probeMax):
+        raise TypeError(f"The given probe length must be a positive integer between {probeMin} and {probeMax}")
+    elif not parse_file_name(filename)[2] in [".ct", ".csv"]:
+        raise TypeError("The given file must have an extension of either .ct or .csv")
 
+    return True
+
+#todo: add support for sscount csv
+def calculate_result(filein, probe_length: int, filename="", arguments = None) -> str:
+    """
+    Find TFO probes
+    This method should be inside a with expression in order to close the given file
+    :param filein: the file object of the file to be read
+    :param probe_length: the length of TFO probes to look for
+    :param filename: the name of the inputted file
+    :param arguments: a dictionary that controls printing during execution
+    :return: str
+    """
     ct_df, structure_count = convert_ct_to_dataframe(filein)
     #todo: ask if can get rid of this and place before
-    if arguments: print('Number of Structures = ' + str(structure_count) + ' \n...Please wait...')
+    if should_print(arguments): print('Number of Structures = ' + str(structure_count) + ' \n\n...Please wait...\n')
 
     sscount_df = getSSCountDF(ct_df, arguments and arguments.emit_sscount)
     consec = get_consecutive_not_ss(probe_length, sscount_df)
@@ -28,41 +45,14 @@ def calculate_result(probe_length: int, filein=None, filename=None, arguments = 
 
     return get_final_string(filename, probe_length, structure_count, consec, sscount_df)
 
-def parseCTFilePath(filein : str, outputDir = None):
+def parse_file_name(filein : str, output_dir = None):
     filein_path = Path(filein).resolve()  # Convert the filein to a Path object and resolve its full path
-    mb_userpath = outputDir or filein_path.parent  # Use the parent directory of the input file to save all files
-    fname = filein_path.stem
-    return (mb_userpath, fname)
+    mb_userpath = output_dir or filein_path.parent  # Use the parent directory of the input file to save all files
+    return mb_userpath, filein_path.stem, filein_path.suffix
 
-# def seqTarget(df: DataFrame): #sequence of target & sscount for each probe as fraction (1 for fully single stranded)
-#     max_base = df.base.iat[-1]
-#     seq = ''.join(df.base)
-#     size = len(df)
-#     return df.sscount, df.baseno, max_base, df.base, seq, size
+def should_print(arguments, is_content_verbose = False):
+    return arguments and arguments.print and not arguments.quiet and (not is_content_verbose or arguments.verbose)
 
-
-basecomplement = str.maketrans({'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'})
-def parallel_complement(seq : str, complement = basecomplement): #generate RNA complement
-    return seq.translate(complement)[::1]
-
-def sequence_probe(baseno: int, probe_len: int, structure_count: int, sscount_df: DataFrame):
-    """
-    Sequence one specific probe
-    :param baseno: an integer representing the start of the probe, 1-indexed
-    :param probe_len: the length of the probe
-    :param sscount_df: the sscount dataframe
-    :return:
-    """
-    probe = "".join(sscount_df.base[baseno-1:baseno+probe_len - 1])
-    complement = parallel_complement(probe)
-
-    tml = int(mt.Tm_NN(complement, dnac1=50000, dnac2=50000, Na=100, nn_table=mt.RNA_NN1, saltcorr=1))
-
-    per = int((probe.count('A') + probe.count('G')) / probe_len * 100)
-
-    probe_sscounts = sscount_df.sscount[baseno - 1:baseno + probe_len - 1]
-    avg_sscount = probe_sscounts.sum() / (probe_len * structure_count)
-    return (baseno, per, avg_sscount, complement, tml) #returns baseno so it can easily be written to file
 
 def get_consecutive_not_ss(probe_length: int, sscount_df : DataFrame) -> DataFrame:
     # get the double stranded elements with bse A or G
@@ -72,35 +62,6 @@ def get_consecutive_not_ss(probe_length: int, sscount_df : DataFrame) -> DataFra
     # get the first element of a 9 length probe
     consec = dscount.loc[dscount.baseno.diff(-1) == -1]  # and the one after
     return consec.loc[consec.baseno.diff(-(probe_length - 2)) == -(probe_length - 2)]
-
-def get_command_line_arguments(args):
-    import argparse, pathlib, functools;
-    def range_type(string, min=probeMin, max=probeMax):
-        value = int(string)
-        if min <= value <= max:
-            return value
-        else:
-            raise argparse.ArgumentTypeError(f'value not in range {min}-{max}. Please either keep it in range or leave it out.')
-        
-    def path_string(string, suffix = ".ct"):
-        path = Path(string).resolve()
-        if path.is_file() and path.suffix == suffix:
-            return str(path)
-        else:
-            raise argparse.ArgumentTypeError(f'Invalid file given. File must be an existing {suffix} file')
-
-    parser = argparse.ArgumentParser(
-                    prog='TDOFinder',
-                    description='Triplex-forming oligonucleotide (TFO) target designer for RNA.')
-    parser.add_argument("-f", "--file", type=path_string)
-    parser.add_argument("-o", "--output-dir", type=functools.partial(path_string, suffix=""))
-    parser.add_argument("-p", "--probes", type=range_type,  metavar=f"[{probeMin}-{probeMax + 1}]", 
-                   help=f'The length of TFO probes, {probeMin}-{probeMax} inclusive')
-    parser.add_argument("-s", "--emit-sscount", action="store_true")
-    parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-q", "--quiet", action="store_true")
-
-    return parser.parse_args(args)
 
 
 def convert_ct_to_dataframe(file):
@@ -112,6 +73,7 @@ def convert_ct_to_dataframe(file):
         """
 
     # dtype={"baseno": int, "base": str, "bs_bind": int}
+    file.seek(0)
     ct_df =  pd.read_csv(file, sep=' +', usecols=[0,1,4], names=["baseno", "base", "bs_bind"], engine='python')
     initial_row_count = len(ct_df)
     ct_df = ct_df[~ct_df["base"].isin(match)]
@@ -138,30 +100,95 @@ def getSSCountDF(ct_dataframe : DataFrame, save_to_file: bool = False) -> DataFr
 
     return df_grouped
 
+
 def get_final_string(file_name : str, probe_length, structure_count, consec, sscount_df):
     to_return = 'Results for ' + file_name + ' using ' + str(probe_length) + ' as parallel TFO probe length\n' + \
                 'Start Position,%GA,sscount,Parallel TFO Probe Sequence,Tm\n'
     get_probe_result = lambda x: ",".join(map(str, sequence_probe(x, probe_length, structure_count, sscount_df)))
-    to_return += "\n".join(map(get_probe_result, consec.baseno))
+    to_return += "\n".join(map(get_probe_result, consec.baseno)) + "\n"
     return to_return
+
+# def seqTarget(df: DataFrame): #sequence of target & sscount for each probe as fraction (1 for fully single stranded)
+#     max_base = df.base.iat[-1]
+#     seq = ''.join(df.base)
+#     size = len(df)
+#     return df.sscount, df.baseno, max_base, df.base, seq, size
+
+def parallel_complement(seq : str, complement = base_complement): #generate RNA complement
+    return seq.translate(complement)[::1]
+
+def sequence_probe(baseno: int, probe_len: int, structure_count: int, sscount_df: DataFrame):
+    """
+    Sequence one specific probe
+    :param baseno: an integer representing the start of the probe, 1-indexed
+    :param probe_len: the length of the probe
+    :param sscount_df: the sscount dataframe
+    :return:
+    """
+    probe = "".join(sscount_df.base[baseno-1:baseno+probe_len - 1])
+    complement = parallel_complement(probe)
+
+    tml = int(mt.Tm_NN(complement, dnac1=50000, dnac2=50000, Na=100, nn_table=mt.RNA_NN1, saltcorr=1))
+
+    per = int((probe.count('A') + probe.count('G')) / probe_len * 100)
+
+    probe_sscounts = sscount_df.sscount[baseno - 1:baseno + probe_len - 1]
+    avg_sscount = probe_sscounts.sum() / (probe_len * structure_count)
+    return (baseno, per, avg_sscount, complement, tml) #returns baseno so it can easily be written to file
+
+
+def get_command_line_arguments(args):
+    import argparse, functools
+    def range_type(string, min=probeMin, max=probeMax):
+        value = int(string)
+        if min <= value <= max:
+            return value
+        else:
+            raise argparse.ArgumentTypeError(
+                f'value not in range {min}-{max}. Please either keep it in range or leave it out.')
+
+    def path_string(string, suffix=".ct"):
+        path = Path(string).resolve()
+        if path.is_file() and path.suffix in suffix:  # in returns true if string ==
+            return str(path)
+        else:
+            raise argparse.ArgumentTypeError(f'Invalid file given. File must be an existing {suffix} file')
+
+    parser = argparse.ArgumentParser(
+        prog='TDOFinder',
+        description='Triplex-forming oligonucleotide (TFO) target designer for RNA.')
+    parser.add_argument("-f", "--file", type=path_string)
+    parser.add_argument("-o", "--output-dir", type=functools.partial(path_string, suffix=""))
+    parser.add_argument("-p", "--probes", type=range_type, metavar=f"[{probeMin}-{probeMax + 1}]",
+                        help=f'The length of TFO probes, {probeMin}-{probeMax} inclusive')
+    parser.add_argument("-s", "--emit-sscount", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("-w", "--overwrite", action="store_true",
+                        help="Overwrite the returned TFO Probes file. Default it to append")
+
+    args = parser.parse_args(args)
+    args.print = True  # denotes that this is from the command line
+    return args
+
 if __name__ == "__main__":
     arguments = get_command_line_arguments(sys.argv[1:])
-    print(arguments)
-    print("\n" * 5)
-    print(" \x1B[3m TFOFinder\x1B[0m  Copyright (C) 2022  Irina E. Catrina\n"
-          "This program comes with ABSOLUTELY NO WARRANTY;\n"
-          "This is free software, and you are welcome to redistribute it\n"
-          "under certain conditions; for details please read the GNU_GPL.txt file.\n\n"
-          "Feel free to use the CLI or to run the program directly with command line arguments \n"
-          "(view available arguments with --help).\n\n"
-
-          f"{"->" * 40}\n\n"
-          "WARNING: Previous files will be overwritten or appended!  Save them in a\n"
-          "different location than the current input file, or rename them.\n\n"
-          f"{"->" * 40}")
+    if should_print(arguments):
+        print("\n" * 5)
+        print(" \x1B[3m TFOFinder\x1B[0m  Copyright (C) 2022  Irina E. Catrina\n"
+              "This program comes with ABSOLUTELY NO WARRANTY;\n"
+              "This is free software, and you are welcome to redistribute it\n"
+              "under certain conditions; for details please read the GNU_GPL.txt file.\n\n"
+              "Feel free to use the CLI or to run the program directly with command line arguments \n"
+              "(view available arguments with --help).\n\n"
+    
+              f"{"->" * 40}\n\n"
+              "WARNING: Previous files will be overwritten or appended!  Save them in a\n"
+              "different location than the current input file, or rename them.\n\n"
+              f"{"->" * 40}")
     
     filein = arguments.file or input('Enter the ct file path and name: ')
-    mb_userpath, fname = parseCTFilePath(filein, arguments.output_dir)
+    mb_userpath, fname, suffix = parse_file_name(filein, arguments.output_dir)
 
     probe_length = arguments.probes or input_int_in_range(min = probeMin, max = probeMax + 1, msg = f"Enter the length of TFO probe; a number between {probeMin} and {probeMax} inclusive: ",
                                fail_message = f'You must type a number between {probeMin} and {probeMax}, try again: ')
@@ -170,7 +197,13 @@ if __name__ == "__main__":
     with open(filein,'r') as file:
         tfo_probes_result = calculate_result(probe_length, file, filename = filein, arguments = arguments)
 
-    #todo: should it be add???
-    #todo: add as row if file already exists
-    with open(mb_userpath / f"{fname}_TFO_probes.txt", 'a') as file:
+    #todo: should it be add
+    #todo: ask if should add extra newline at end (trivial issue)
+    with open(mb_userpath / f"{fname}_TFO_probes.txt", "w" if arguments.overwrite else 'a') as file:
         file.write(tfo_probes_result)
+
+    if should_print(arguments, is_content_verbose = True):
+        import textwrap
+        print("Results: \n" + textwrap.indent(tfo_probes_result, " " * 4))
+    if should_print(arguments):
+        print("Calculation complete. Find the result in " + str(mb_userpath / f"{fname}_TFO_probes.txt"))
